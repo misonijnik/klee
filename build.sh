15c0b03602c98ef64cf28cb2d9c1f21a07ce7f95
Saveliy Grigoryev <sava-cska@mail.ru>
2022-07-20 19:54:26 +0300

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script is used to build KLEE as UTBot backend
+
+set -e
+set -o pipefail
+mkdir -p build
+cd build
+$UTBOT_CMAKE_BINARY -G Ninja \
+  -DCMAKE_PREFIX_PATH=$UTBOT_INSTALL_DIR/lib/cmake/z3 \
+  -DCMAKE_LIBRARY_PATH=$UTBOT_INSTALL_DIR/lib \
+  -DCMAKE_INCLUDE_PATH=$UTBOT_INSTALL_DIR/include \
+  -DENABLE_SOLVER_Z3=ON \
+  -DENABLE_POSIX_RUNTIME=ON \
+  -DENABLE_FLOATING_POINT=ON \
+  -DENABLE_FP_RUNTIME=ON \
+  -DLLVM_CONFIG_BINARY=$UTBOT_INSTALL_DIR/bin/llvm-config \
+  -DENABLE_UNIT_TESTS=OFF \
+  -DENABLE_SYSTEM_TESTS=OFF \
+  -DGTEST_SRC_DIR=$UTBOT_ALL/gtest \
+  -DGTEST_INCLUDE_DIR=$UTBOT_ALL/gtest/googletest/include \
+  -DCMAKE_INSTALL_PREFIX=$UTBOT_ALL/klee/ \
+    ..
+$UTBOT_CMAKE_BINARY --build .
+$UTBOT_CMAKE_BINARY --install .
+
