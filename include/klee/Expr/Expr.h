8c89396296853f72f1bd49cf1bdd16938929c779
Alex Babushkin <ocelaiwo@gmail.com>
2022-07-29 13:48:34 +0300

    aa

--- include/klee/Expr/Expr.h
+++ include/klee/Expr/Expr.h
@@ -218,6 +218,9 @@ public:
   /// dump - Print the expression to stderr.
   void dump() const;
 
+  // Get string representation of the expression.
+  std::string toString() const;
+
   /// Returns the pre-computed hash of the current expression
   virtual unsigned hash() const { return hashValue; }
 
