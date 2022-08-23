cdf43875b26e1cb938eb7466c6662b641d60e2d5
Aleksandr Misonizhnik <misonijnik@gmail.com>
2022-08-23 13:54:13 +0300

    [fix] base can be concrete

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -2947,7 +2947,7 @@ void Executor::executeInstruction(ExecutionState &state, KInstruction *ki) {
     if (kgepi->offset)
       offset = AddExpr::create(offset, Expr::createPointer(kgepi->offset));
     ref<Expr> address = AddExpr::create(base, offset);
-    if (UseGEPExpr && !isa<ConstantExpr>(address) && !isa<ConstantExpr>(base)) {
+    if (UseGEPExpr && !isa<ConstantExpr>(address)) {
       if (isGEPExpr(base))
         gepExprBases[address] = gepExprBases[base];
       else
