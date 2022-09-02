2d00585e38be08af0d32e6d565e39d68ed31b9be
Aleksandr Misonizhnik <misonijnik@gmail.com>
2022-09-02 13:05:43 +0300

    [feat] Improve perfomance

--- lib/Core/Searcher.cpp
+++ lib/Core/Searcher.cpp
@@ -265,6 +265,9 @@ TargetedSearcher::tryGetWeight(ExecutionState *es, double &weight) {
       bb = kb->basicBlock;
     }
     sfNum++;
+
+    if (minCallWeight < sfNum)
+      break;
   }
 
   WeightResult res = Miss;
