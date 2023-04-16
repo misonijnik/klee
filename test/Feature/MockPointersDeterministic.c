f9f68deb1cc62db2e2ec615b6d963529888a55b4
Aleksandr Misonizhnik <misonijnik@gmail.com>
2023-04-17 01:32:55 +0300

    [fix] tests

--- test/Feature/MockPointersDeterministic.c
+++ test/Feature/MockPointersDeterministic.c
@@ -1,13 +1,11 @@
 // REQUIRES: z3
 // RUN: %clang %s -g -emit-llvm %O0opt -c -o %t.bc
-
 // RUN: rm -rf %t.klee-out-1
 // RUN: %klee --solver-backend=z3 --use-independent-solver=false --output-dir=%t.klee-out-1 --skip-not-lazy-initialized --external-calls=all --mock-strategy=deterministic %t.bc 2>&1 | FileCheck %s -check-prefix=CHECK-1
 // CHECK-1: memory error: null pointer exception
 // CHECK-1: KLEE: done: completed paths = 1
-// CHECK-1: KLEE: done: partially completed paths = 1
-// CHECK-1: KLEE: done: generated tests = 2
-// XFAIL: *
+// CHECK-1: KLEE: done: partially completed paths = 2
+// CHECK-1: KLEE: done: generated tests = 3
 
 #include <assert.h>
 
