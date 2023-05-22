9f7135793b220a421a3b65a5071cfc0916579d63
Victor Samoilov <victor.samoilov.1234@gmail.com>
2023-05-22 16:10:23 +0300

    Factor out distance calculations for searchers in DistanceCalculator
    Fix style
    
    Add TargetReachability to update reachability of targets
    
    Fix current confidence rates: check reachability for stucked states, decrease from solver timeouts and divide more during branch
    
    Add skeleton for moving logic from searcher, without CoverageGuidance
    
    Move isStuck to ExecutionState
    
    Add handling of targetless states and reachedTargets
    
    Add block method in target forest
    
    Add history in ExecutionState
    
    Add enum for guidance
    
    Add first version of moving forest and distance calculation logic from searcher
    
    Change ExecutionState * to uint32_t in reachability
    
    Change interface of DistanceCalculator and pointer to reference
    
    Move heuristic to distanceCalculator
    
    Format
    
    Fix
    
    [fix]

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -13,6 +13,7 @@
 #include "CXXTypeSystem/CXXTypeManager.h"
 #include "Context.h"
 #include "CoreStats.h"
+#include "DistanceCalculator.h"
 #include "ExecutionState.h"
 #include "ExternalDispatcher.h"
 #include "GetElementPtrTypeIterator.h"
@@ -446,6 +447,7 @@ Executor::Executor(LLVMContext &ctx, const InterpreterOptions &opts,
       specialFunctionHandler(0), timers{time::Span(TimerInterval)},
       concretizationManager(new ConcretizationManager(EqualitySubstitution)),
       codeGraphDistance(new CodeGraphDistance()),
+      distanceCalculator(new DistanceCalculator(*codeGraphDistance)),
       targetedExecutionManager(*codeGraphDistance), replayKTest(0),
       replayPath(0), usingSeeds(0), atMemoryLimit(false), inhibitForking(false),
       haltExecution(HaltExecution::NotHalt), ivcEnabled(false),
@@ -4182,7 +4184,7 @@ void Executor::targetedRun(ExecutionState &initialState, KBlock *target,
   states.insert(&initialState);
 
   TargetedSearcher *targetedSearcher =
-      new TargetedSearcher(Target::create(target), *codeGraphDistance);
+      new TargetedSearcher(Target::create(target), *distanceCalculator);
   searcher = targetedSearcher;
 
   std::vector<ExecutionState *> newStates(states.begin(), states.end());
@@ -4522,6 +4524,8 @@ void Executor::terminateStateOnExecError(ExecutionState &state,
 void Executor::terminateStateOnSolverError(ExecutionState &state,
                                            const llvm::Twine &message) {
   terminateStateOnError(state, message, StateTerminationType::Solver, "");
+  SetOfStates states = {&state};
+  decreaseConfidenceFromStoppedStates(states, HaltExecution::MaxSolverTime);
 }
 
 // XXX shoot me
