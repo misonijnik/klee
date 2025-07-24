1cd51d7a64a583594ed9c1ddce4fe4765efeba43
metametamoon <sorvinlevs@gmail.com>
2025-07-24 16:06:44 +0300

    fix: test headers

--- test/Bidirectional/Memory/stack-pointer-change-2.c
+++ test/Bidirectional/Memory/stack-pointer-change-2.c
@@ -1,6 +1,4 @@
-// checks that symbolic memory works in bidirectional mode
-// it does not; requires a fix
-
+// REQUIRES: not-metasmt
 // RUN: %clang %s -emit-llvm %O0opt -c -fno-discard-value-names -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --write-kqueries --output-dir=%t.klee-out --max-propagations=5 --max-stack-frames=15 --execution-mode=bidirectional --tmp-skip-fns-in-init=false --initialize-in-join-blocks --function-call-reproduce=reach_error --skip-not-lazy-initialized --forward-ticks=0 --backward-ticks=5 --skip-not-symbolic-objects --write-xml-tests --debug-log=rootpob,backward,conflict,closepob,reached,init --debug-constraints=backward %t.bc 2> %t.log
